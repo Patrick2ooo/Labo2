a644c3a1322905d7e301419347f034e920b4603b
Patrick2ooo <p.maillard1522@gmail.com>
2023-04-27 15:30:53 +0200

    listes_dynamiques.c

---
+++ listes_dynamiques.c
@@ -0,0 +1,14 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : listes_dynamiques.c
+ Auteur(s)      : René Rentsch
+ Date creation  : 18.04.2023
+
+ Description    : Librairie permettant la gestion de listes doublement chaînées
+                  non circulaires
+
+ Remarque(s)    : -
+
+ Compilateur    : Mingw-w64 gcc 12.2.0
+ -----------------------------------------------------------------------------------
+*/
